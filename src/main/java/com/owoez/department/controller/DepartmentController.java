cfe54513e4a4b45db4260e0ba5e84403146c28e0
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2021-08-04 15:20:41 +0100

    Saving department

--- src/main/java/com/owoez/department/controller/DepartmentController.java
+++ src/main/java/com/owoez/department/controller/DepartmentController.java
@@ -1,11 +1,24 @@
 package com.owoez.department.controller;
 
+import com.owoez.department.entity.Department;
 import com.owoez.department.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/departments")
+@Slf4j
 public class DepartmentController {
   @Autowired
   private DepartmentService departmentService;
+
+  @PostMapping("/")
+  public Department saveDepartment(@RequestBody Department department){
+    log.info("Inside saveDepartment method of Department Controller");
+    return departmentService.saveDepartment(department);
+  }
 }
--- src/main/java/com/owoez/department/service/DepartmentService.java
+++ src/main/java/com/owoez/department/service/DepartmentService.java
@@ -1,12 +1,19 @@
 package com.owoez.department.service;
 
+import com.owoez.department.entity.Department;
 import com.owoez.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class DepartmentService {
   @Autowired
   private DepartmentRepository departmentRepository;
 
+  public Department saveDepartment(Department department) {
+    log.info("Inside saveDepartment of departmentService");
+    return departmentRepository.save(department);
+  }
 }
